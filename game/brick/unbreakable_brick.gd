934554e3fa961c96520173ad02a61df24b289b80
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-10-28 19:57:52 +0100

    Add unbreakable bricks

---
+++ game/brick/unbreakable_brick.gd
@@ -0,0 +1,9 @@
+# Copyright © 2018 Hugo Locurcio and contributors - MIT License
+# See `LICENSE.md` included in the source distribution for details.
+
+extends "res://game/brick/brick.gd"
+class_name UnbreakableBrick
+
+func destroy():
+	# Brick is unbreakable, do nothing
+	pass
