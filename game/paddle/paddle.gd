64056741d221138d1cfc3cffed7464b58977cbf4
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-10-30 18:42:32 +0100

    Remove unused `_ready()` function declarations

--- game/paddle/paddle.gd
+++ game/paddle/paddle.gd
@@ -4,9 +4,6 @@
 extends KinematicBody2D
 class_name Paddle
 
-func _ready() -> void:
-	pass
-
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		position.x = event.position.x
--- menu/main.gd
+++ menu/main.gd
@@ -3,9 +3,6 @@
 
 extends Control
 
-func _ready() -> void:
-	pass
-
 func _on_play_pressed() -> void:
 	get_tree().change_scene("res://game/game.tscn")
 
